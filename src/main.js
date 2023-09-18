d2cde587a89eb03ce2bc2b04a72446c16b7de078
qjjjn <1103832843@qq.com>
2023-09-18 22:12:16 +0800

    调整目录

--- src/main.js
+++ src/main.js
@@ -1,13 +1,12 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import '@/assets/main.scss'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
-console.log(gaga)
+
 app.use(createPinia())
 app.use(router)
 
